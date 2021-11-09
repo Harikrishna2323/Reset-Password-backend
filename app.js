bb41cf0c4d113b4bf1eed9059513d663732f6c7f
harikrishna2323 <harikb23@gmail.com>
2021-11-09 19:28:08 +0530

    added cookieParser and cors

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/auth");
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.use();
 
 app.use("/", authRouter);
 
